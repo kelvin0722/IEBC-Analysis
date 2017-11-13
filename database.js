95323c3d36060713061c00ea182d1eec3302662d
kelvin <kelvinm442@gmail.com>
2017-11-13 19:39:05 +0300

    add MongoClient to handle database connection and insertions

--- database.js
+++ database.js
@@ -1,20 +1,46 @@
-require('dotenv').config()
+require('dotenv').config();
 
 var fs = require('graceful-fs');
 const dookie = require('dookie');
 const glob = require('glob');
 
+
+var MongoClient = require('mongodb').MongoClient;
 //mongodb connection string
 //create a sample db in mongodb
-const connectionUrl = process.env.DATABASE_URL || 'mongodb://localhost:27017/test'
+const connectionUrl =
+  process.env.DATABASE_URL || 'mongodb://localhost:27017/test';
 
-glob("./datafiles/*.json",(err,files)=>{
-  files.map((file)=>{
-    console.log(file);
-      let data = JSON.parse(fs.readFileSync(file, 'utf8'));
-      console.log(data);
-      dookie.push(connectionUrl, data).then(()=> {
-        console.log('done!');
-      },(err)=>console.log(err));
-  })
-})
+MongoClient.connect(connectionUrl, (err, db)=>{
+  if(err){ 
+    console.log("Please check you db connection parameters");
+  }else{
+    console.log("Connection success");
+    // here we are going to write code for file
+    glob('./datafiles/BOJ_GET/*.json', (err, files) => {
+    files.map(file => {
+    fs.readFile('DATA', 'utf8', (err, contents) => {
+    console.log(contents);
+   });
+    let data = JSON.parse(fs.readFileSync(file, 'utf8'));
+    console.log(data);
+    var collection = db.collection('Candidate_Results');
+    collection.insert(data, (err, result) => {
+      if(err){
+        console.log(err);
+      }else {
+        console.log(result);
+      }
+    });
+  });
+
+});
+  }
+});
+
+
+
+
+
+
+
